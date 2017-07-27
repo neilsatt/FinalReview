0a081c4ebffba29033ee999777a0cd84b7cdf226
neilsatt <neilsatt@gmail.com>
2017-07-26 20:08:00 -0600

    initial js loops page

---
+++ JS/Loops.js
@@ -0,0 +1,21 @@
+/* While Loops */
+var counter = 1;
+while ( counter <= 10 ) {
+  console.log(counter);
+  counter += 1;
+}
+
+
+/* Do while loops */
+var result = '';
+var i = 0;
+do {
+   result += "The number is: "+i + '\n'; // newline for console
+   i++; 
+} while (i < 5);
+console.log(result);
+
+
+
+
+
