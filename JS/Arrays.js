349c46df8a949c0438e9e14e5fe985ad377eee38
neilsatt <neilsatt@gmail.com>
2017-07-27 18:23:25 -0600

    add array page

---
+++ JS/Arrays.js
@@ -0,0 +1,10 @@
+/* Arrays-  a data structure for holding multiple pieces of information */
+
+var scores = [100, 99, 99, 72, 60]; // numbers 
+var names = ['Joan', 'Amit', 'Sarah', 'Ricardo', 'Piers']; // strings
+var values = [1, -100, true, false, 'JavaScript'];  // mixed values 
+
+/* Use and Index Value to acess individual items */
+console.log(scores[3]); // 72
+
+/* Array Indices */
