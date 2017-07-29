d023f04bdc91b482d62b69e900eb10a0f122fcd8
neilsatt <neilsatt@gmail.com>
2017-07-28 20:04:08 -0600

    finish arrays

--- JS/Quiz.js
+++ JS/Quiz.js
@@ -10,8 +10,22 @@ var answer;
 var response;
 var html;
 
+//update to show correct and incorrect answers
+var correct = [];
+var wrong = [];
+
 function print(message) {
-    document.write(message);
+    var outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = message;
+}
+
+function buildList(arr){
+    var listHTML = '<ol>';
+    for (var i = 0; i < arr.length; i++){
+        listHTML += '<li>' + arr[i] + '</li>';
+    }
+    listHTML += '</ol>';
+    return listHTML;   
 }
 
 for(var i = 0; i < questions.length; i++) {
@@ -20,8 +34,15 @@ for(var i = 0; i < questions.length; i++) {
     response = parseInt(prompt(question));
     if (response === answer){
         correctAnswers += 1;
+        correct.push(question); // add the correct answer to the array 
+    } else {
+        wrong.push(question); // add the incorrect answer to the array 
     }
 }
 
 html = "You got "+correctAnswers + " question(s) right.";
+html += '<h2>You got these questions correct:</h2>';
+html += buildList(correct);
+html += '<h2>You got these questions wrong:</h2>';
+html += buildList(wrong);
 print(html);
