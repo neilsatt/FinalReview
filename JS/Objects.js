3d640c9bddaae2a162e34d92dbf9b9e30e12b04d
neilsatt <neilsatt@gmail.com>
2017-07-30 14:56:39 -0600

    updates to objects section

--- JS/Objects.js
+++ JS/Objects.js
@@ -4,4 +4,27 @@ key: value
 property: value
 
 // example of object literal
-var student = {}
+var student = {
+    name: "dave",
+    grades: [80, 67, 98, 86, 89], 
+}
+
+// Access using brackets (not as common)
+student['name';]
+
+// Access using dot notation
+student.name; // dave
+
+// Add new properties 
+student.gradeLevel = 'junior';
+
+/* Looping through items in an object */
+for (var key in student) { // var name (key) can be whatever you chose
+    console.log(key); // would print out 'name' and 'grades'
+}
+
+// Print out key AND value
+for (prop in student) { // this time use prop as the variable, just because
+    console.log(prop, ': ', student[prop]); // use a comma for the value
+    // and then you HAVE to use bracket notation to get the prop value 
+}
