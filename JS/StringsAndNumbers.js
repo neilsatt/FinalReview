e2b913cc1f42d79d5d3b9731c194121409db1a92
neilsatt <neilsatt@gmail.com>
2017-07-30 19:59:29 -0600

    update numbers page

---
+++ JS/StringsAndNumbers.js
@@ -0,0 +1,56 @@
+/*  
+    Strings
+    Used for words, sentences and other text. Used inside quote marks. 
+    
+    To combine strings, use concatenation ('hello '+ 'there')
+        or
+    Use the += sign
+    message = 'Hi there';
+    message += ' what is your name';
+    
+    Common String methods:
+    length()
+    toUpperCase()
+    
+    
+*/
+
+
+/*  Numbers
+    Used for making calculations - adding, subtracting, multiplying, etc.
+    
+    Integers = 5, 0, -100, etc.
+    Floating Point Numbers = 3.14, -9.88. .009, etc. 
+    Scientific = 9e-6
+    
+    
+    Math Object and Methods
+    Math.round(2.2) // 2
+    Math.round(44.9) // 45
+    Math.floor(1.2) // 1
+    Math.ceil(1.2) // 2
+    
+    Create a Random Number 
+    Math.random() - returns a floating num from 0 up to but not including 1
+    
+    // Get a random number from 1 to 6 
+    Math.random() * 6; // give you numbers between 0 and 5, with decimals
+    Math.floor( Math.random() * 6); // gives you random even numbers betwen 0 and 6
+    var dieRoll = Math.floor( Math.random() * 6) + 1; // gives you random even numbers betwen 1 and 6
+    console.log(dieRoll);
+    
+    
+    
+    
+*/
+
+/* Methods for Strings/Numbers 
+    parseInt() - convert string to Number
+    var str = '49';
+    var num = parseInt(str);
+
+    parseFloat() - convert string to floating number
+    var str = '102.99%';
+    var num = parseFloat(str);
+    
+*/
